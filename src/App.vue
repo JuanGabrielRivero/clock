118c527ab4cda93374ef1fe89fc006c6b198178e
Juani <jrm3301@gmail.com>
2022-05-11 16:14:08 +0100

    crear proyecto reloj con vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import VueWatch from "./components/ VueWatch.vue";
+</script>
+
+<template>
+  <VueWatch />
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Wallpoet&display=swap");
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+@font-face {
+  font-family: "alarm clock";
+  src: url(./fonts/alarm.ttf);
+}
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "BaseWelcome",
+};
+</script>
+
+<template>
+  <h1>Welcome</h1>
+  <p>Esto es una prueba de Vue</p>
+</template>
+
+<style>
+h1 {
+  color: green;
+}
+</style>
